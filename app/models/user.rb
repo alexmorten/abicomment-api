dafe67c3a8476701223b09178a8946020de542de
Eugen Martin <eugeniusmartinus@gmail.com>
2016-04-02 19:31:31 +0200

    add devise to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_many :comments
+
+  # notice this comes BEFORE the include statement below
+  # also notice that :confirmable is not included in this block
+  devise :database_authenticatable,
+    :recoverable,
+    :trackable,
+    :validatable,
+    :registerable
+    # ,:omniauthable
+
+  # note that this include statement comes AFTER the devise block above
+  include DeviseTokenAuth::Concerns::User
 end
