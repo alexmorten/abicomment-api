4c85d15e9360d74f2b1ab9eefb7e08ed3c71da97
alexandrius.martinus@gmx.de <alex>
2016-06-08 16:34:55 +0200

    not working yet

--- app/serializers/poll_serializer.rb
+++ app/serializers/poll_serializer.rb
@@ -1,4 +1,8 @@
 class PollSerializer < ActiveModel::Serializer
-  attributes :id, :topic, :voteamount
+  attributes :id, :topic, :voteamount , :hasvoted
   has_many :options
+
+def hasvoted
+  true if scope.current_user.voted_for?(object)
+end
 end
