9e8a060a5e64a84dd09676805d5a9629d85c5f52
alexandrius.martinus@gmx.de <alex>
2016-06-09 17:59:20 +0200

    First tests

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name,:forename, :age,:status
-  has_many :comments
+  has_many :ordered_comments ,key:"comments"
 
-
-
+  def ordered_comments
+    object.comments.order(created_at: :desc)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,39 @@
-u1 = User.create(forename:"Alex",name:"Martin",email:"abc@email.com",password:"123456789")
+# u1 = User.create(forename:"Alex",name:"Martin",email:"abc@email.com",password:"123456789")
+#
+# u2 = User.create(forename:"Max",name:"Mustermann",email:"def@email.com",password:"123456789")
+# u2 = User.create(forename:"Tim",name:"Mustermann",email:"def1@email.com",password:"123456789")
+# u2 = User.create(forename:"Felix",name:"Mustermann",email:"def2@email.com",password:"123456789")
+# u2 = User.create(forename:"Tom",name:"Mustermann",email:"def3@email.com",password:"123456789")
+# u2 = User.create(forename:"Sigmund",name:"Mustermann",email:"def4@email.com",password:"123456789")
+# u2 = User.create(forename:"Phillip",name:"Mustermann",email:"def5@email.com",password:"123456789")
+# u2 = User.create(forename:"Vera",name:"Mustermann",email:"def6@email.com",password:"123456789")
+# u2 = User.create(forename:"Krassus",name:"Mustermann",email:"def7@email.com",password:"123456789")
+# u2 = User.create(forename:"Noch Jemand",name:"Mustermann",email:"def8@email.com",password:"123456789")
+#
+#  q = Quote.create(text:"wir machen hier keine Intervall-Rechnung!",quoted:"Herr Berning",user:u1)
+#
+#
+# p1 = Poll.create(topic:"Wer wird der naechste Bundeskanzler?")
+#   v11=Option.create(title:"max",poll:p1)
+#   v12=Option.create(title:"tom",poll:p1)
+#
+# p2 = Poll.create(topic:"Wer gewinnt?")
+#   v21=Option.create(title:"Niemand",poll:p2)
+#   v22=Option.create(title:"Jemand",poll:p2)
+# p3 = Poll.create(topic:"Wer kuemmert sich um die umfragen?")
+#   v31=Option.create(title:"Keiner",poll:p3)
+#   v32=Option.create(title:"Ich",poll:p3)
+#
+#
 
-u2 = User.create(forename:"Max",name:"Mustermann",email:"def@email.com",password:"123456789")
-u2 = User.create(forename:"Tim",name:"Mustermann",email:"def1@email.com",password:"123456789")
-u2 = User.create(forename:"Felix",name:"Mustermann",email:"def2@email.com",password:"123456789")
-u2 = User.create(forename:"Tom",name:"Mustermann",email:"def3@email.com",password:"123456789")
-u2 = User.create(forename:"Sigmund",name:"Mustermann",email:"def4@email.com",password:"123456789")
-u2 = User.create(forename:"Phillip",name:"Mustermann",email:"def5@email.com",password:"123456789")
-u2 = User.create(forename:"Vera",name:"Mustermann",email:"def6@email.com",password:"123456789")
-u2 = User.create(forename:"Krassus",name:"Mustermann",email:"def7@email.com",password:"123456789")
-u2 = User.create(forename:"Noch Jemand",name:"Mustermann",email:"def8@email.com",password:"123456789")
+----
+u = User.create(forename:"Alexander",name:"Martin",email:"alexandrius.martinus@gmx.de",password:"1234567890")
+ u = User.create(forename:"Leon",name:"Berkemeier",email:"Leonxd90411@gmail.com",password:"zJg4Hd9c")
+ u = User.create(forename:"Christopher",name:"Kröger",email:"chrissi12l@web.de",password:"pdkx7PRT")
+u = User.create(forename:"Max",name:"Kolvenbach",email:"maxkolvenbach99@gmail.com",password:"8dVqeR6X")
+ u = User.create(forename:"Keno",name:"Juist",email:"keno.juist@gmail.com",password:"R4NnmA6G")
+  u = User.create(forename:"Simon",name:"Thiele",email:"sth@mplogin.de",password:"T5cRAC2g")
+  u = User.create(forename:"Celine",name:"Middendorf",email:"celine@cmiddendorf.de",password:"nVhVAA7j")
 
- q = Quote.create(text:"wir machen hier keine Intervall-Rechnung!",quoted:"Herr Berning",user:u1)
-
-
-p1 = Poll.create(topic:"Wer wird der naechste Bundeskanzler?")
-  v11=Option.create(title:"max",poll:p1)
-  v12=Option.create(title:"tom",poll:p1)
-
-p2 = Poll.create(topic:"Wer gewinnt?")
-  v21=Option.create(title:"Niemand",poll:p2)
-  v22=Option.create(title:"Jemand",poll:p2)
-p3 = Poll.create(topic:"Wer kuemmert sich um die umfragen?")
-  v31=Option.create(title:"Keiner",poll:p3)
-  v32=Option.create(title:"Ich",poll:p3)
+  #  u = User.create(forename:"Leon",name:"Berkemeier",email:"Leonxd90411@gmail.com",password:"zJg4Hd9c")
+    # u = User.create(forename:"Leon",name:"Berkemeier",email:"Leonxd90411@gmail.com",password:"zJg4Hd9c")
