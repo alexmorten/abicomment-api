2e38bb7a3f1c3fbdfe8718099243fce162c4b5c9
alexandrius.martinus@gmx.de <alex>
2016-06-13 22:58:05 +0200

    fixed serializers

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,5 +1,8 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :teacher, :name
+  attributes :id, :teacher, :name, :isattending
   has_many :attendings
-  
+  has_many :anecdotes
+  def isattending
+    scope.is_attending?(object)
+  end
 end
