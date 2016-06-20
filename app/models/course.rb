5b4e7e467bd4bed7d0645c77d58e59e55d208f90
alexandrius.martinus@gmx.de <alex>
2016-06-20 15:57:30 +0200

    fix for poll.creator

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-  belongs_to :creator, :class_name => "User", :inverse_of => :courses_created
+  belongs_to :creator, :class_name => "User", :inverse_of => :courses_created,  foreign_key: "user_id"
   has_many :attendings, dependent: :destroy
   has_many :users, through: :attendings
   has_many :anecdotes, dependent: :destroy
