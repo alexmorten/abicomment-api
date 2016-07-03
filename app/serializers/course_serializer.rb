3be83b925b69b6c9a3e0623a0ecd4a29c2b303cb
alexandrius.martinus@gmx.de <alex>
2016-07-03 18:04:55 +0200

    creator to course serialiser

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -2,6 +2,7 @@ class CourseSerializer < ActiveModel::Serializer
   attributes :id, :teacher, :name, :isattending
   has_many :attendings
   has_many :anecdotes
+  belongs_to :creator
   def isattending
     scope.is_attending?(object)
   end
