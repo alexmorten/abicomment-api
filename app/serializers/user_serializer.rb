a79b89461f083091adb9983cbe7a82559c271db8
alexandrius.martinus@gmx.de <alex>
2016-07-12 16:34:53 +0200

    stats for admins

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name,:forename,:fullname,:status
+  attributes :id, :name,:forename,:fullname,:status,:currentsigninat,:currentsigninip
   def fullname
     object.forename+" "+object.name
+  end
+  def currentsigninat
+    object.current_sign_in_at
+  end
+  def currentsigninip
+    object.current_sign_in_ip
   end
 end
