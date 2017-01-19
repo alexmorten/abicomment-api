9bbb057a5a1266608af1df3dfa7d740010a0326f
alexmorten <alexandrius.martinus@gmx.de>
2017-01-20 00:52:52 +0100

    removed user from transaction serializer

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,6 +1,6 @@
 class TransactionSerializer < ActiveModel::Serializer
   attributes :id, :reason, :receipt, :excerpt, :cardnumber, :value, :date, :balance
-  has_one :user
+  
   def balance
     Transaction.where("id <= ?",object.id).sum(:value).to_f
   end
