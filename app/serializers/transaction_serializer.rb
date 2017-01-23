650fce65b6a0604bb0bb02ebbee00492ee04e01c
alexmorten <alexandrius.martinus@gmx.de>
2017-01-23 01:15:00 +0100

    fixed sql where with date

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -2,6 +2,6 @@ class TransactionSerializer < ActiveModel::Serializer
   attributes :id, :reason, :receipt, :excerpt, :cardnumber, :value, :date, :balance
 
   def balance
-    Transaction.where("date <= ?",object.date).sum(:value).to_f
+    Transaction.where("DATE(date) <= ?",object.date).sum(:value).to_f
   end
 end
