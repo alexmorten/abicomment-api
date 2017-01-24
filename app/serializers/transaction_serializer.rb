f5260c48be08937dd8f997fbeddc0c274c502939
alexmorten <alexandrius.martinus@gmx.de>
2017-01-24 13:42:58 +0100

    added user to transaction serializer

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,6 +1,6 @@
 class TransactionSerializer < ActiveModel::Serializer
   attributes :id, :reason, :receipt, :excerpt, :cardnumber, :value, :date, :balance
-
+  belongs_to :user
   def balance
     Transaction.where("DATE(date) <= ?",object.date).sum(:value).to_f
   end
