892d802542e40d4fdaece2963169be5f0f511941
alexmorten <alexandrius.martinus@gmx.de>
2017-07-24 17:07:20 +0200

    removed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,1 @@
 
-
-u = User.create(forename:"Alexander",name:"Martin",email:"alexandrius.martinus@gmx.de",password:"1234567890")
- u = User.create(forename:"Leon",name:"Berkemeier",email:"Leonxd90411@gmail.com",password:"zJg4Hd9c")
- u = User.create(forename:"Christopher",name:"Kröger",email:"chrissi12l@web.de",password:"pdkx7PRT")
-u = User.create(forename:"Max",name:"Kolvenbach",email:"maxkolvenbach99@gmail.com",password:"8dVqeR6X")
- u = User.create(forename:"Keno",name:"Ohrmann",email:"keno.juist@gmail.com",password:"R4NnmA6G")
-  u = User.create(forename:"Simon",name:"Thiele",email:"sth@mplogin.de",password:"T5cRAC2g")
-  u = User.create(forename:"Celine",name:"Middendorf",email:"celine@cmiddendorf.de",password:"nVhVAA7j")
-u = User.create(forename:"Ines",name:"Lütke Wenning",email:"ineslw@web.de",password:"12345678")
-u = User.create(forename:"Andre",name:"Felmet",email:"a.felmet@online.de",password:"xFce31fjiurd")
-u = User.create(forename:"Steffen",name:"Beckmann",email:"steffen.beckmann1@gmail.com",password:"jxUT2Z8L")
